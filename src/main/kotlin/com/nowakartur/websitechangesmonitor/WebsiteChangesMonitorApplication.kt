15c578f063466f9f167035d438560e2bdfebb4b3
NowakArtur97 <nowart97@gmail.com>
2024-10-10 16:58:00 +0200

    Initialize project using Spring Initializr

---
+++ src/main/kotlin/com/nowakartur/websitechangesmonitor/WebsiteChangesMonitorApplication.kt
@@ -0,0 +1,11 @@
+package com.nowakartur.websitechangesmonitor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsiteChangesMonitorApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebsiteChangesMonitorApplication>(*args)
+}
---
+++ src/test/kotlin/com/nowakartur/websitechangesmonitor/WebsiteChangesMonitorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nowakartur.websitechangesmonitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsiteChangesMonitorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
